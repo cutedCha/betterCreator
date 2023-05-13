51ef444a69e9c59fb14c4400f9e606e2b5c00b0c
smile <smile.m@binarywalk.com>
2023-05-13 15:23:49 +0800

    feat:新增接口

--- src/betterCreator/BetterCreator.ts
+++ src/betterCreator/BetterCreator.ts
@@ -1,3 +1,16 @@
+import { IStageConfig } from "./BetterCreatorEvn";
+
 export class BetterCreator {
     public stage: any = null; //舞台的节点
+    /**
+     * 初始化引擎。使用引擎需要先初始化引擎，否则可能会报错。
+     */
+    static init(stageConfig?: IStageConfig): Promise<void> {}
+    /**
+     * @param width
+     * @param height
+     */
+    static init(width: number, height: number) {}
+    /**@private Render 类的引用。*/
+    static render: any;
 }
---
+++ src/betterCreator/BetterCreatorEvn.ts
@@ -0,0 +1,9 @@
+export interface IStageConfig {
+    designWidth?: number;
+    designHeight?: number;
+    scaleMode?: string;
+    screenMode?: string;
+    alignV?: string;
+    alignH?: string;
+    backgroundColor?: string;
+}
